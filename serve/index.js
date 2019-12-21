f3deeb4461110720642890f0f6b504febd4d4d10
xn1254 <passangerxu@gmail.com>
2019-12-22 02:47:36 +0800

    no message

--- serve/index.js
+++ serve/index.js
@@ -13,6 +13,7 @@ app.set('secret', 'safsdf')
 
 app.use(express.json())
 app.use(require('cors')())
+console.log(123)
 // express托管静态文件
 app.use('/uploads', express.static(__dirname + '/uploads'))
 app.use('/admin', express.static(__dirname + '/admin'))
